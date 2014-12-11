b84779b42189e85644dfead9491cf21e4e6372d7
Rajesh <sahoorajesh.d@gmail.com>
2014-12-11 09:10:39 +0530

    [hello] added hello from gstreamer online tutorial

---
+++ hello/hello.c
@@ -0,0 +1,34 @@
+/**
+ This example available online in gstreamer tutorial helloworld
+ TO COMPILE:
+	gcc hello.c `pkg-config gstreamer-1.0 --cflags --libs` -o hello
+**/
+#include <gst/gst.h>
+   
+int main(int argc, char *argv[]) {
+  GstElement *pipeline;
+  GstBus *bus;
+  GstMessage *msg;
+   
+  /* Initialize GStreamer */
+  gst_init (&argc, &argv);
+   
+  /* Build the pipeline */
+  // small change as I have only plybin in my system so i canged playbin2 to playbin
+  pipeline = gst_parse_launch ("playbin uri=http://docs.gstreamer.com/media/sintel_trailer-480p.webm", NULL);
+   
+  /* Start playing */
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+   
+  /* Wait until error or EOS */
+  bus = gst_element_get_bus (pipeline);
+  msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+   
+  /* Free resources */
+  if (msg != NULL)
+    gst_message_unref (msg);
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+  return 0;
+}
